331066fecde646b37455f3876a9b5dae479e09d2
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-01-14 12:30:59 +0530

    Initial commit --- working

---
+++ app/src/main/java/rm/iamjosephvarghese/musicplayer/MainActivity.java
@@ -0,0 +1,45 @@
+package rm.iamjosephvarghese.musicplayer;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+
+
+
+        final MediaPlayer ring= MediaPlayer.create(MainActivity.this,R.raw.got);
+        ring.setLooping(true);
+        ring.start();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(ring.isPlaying()){
+                    ring.pause();
+                }else{
+                    ring.start();
+                }
+
+
+            }
+        });
+
+    }
+}
